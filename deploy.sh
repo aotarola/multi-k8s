ea0282568cc9f8a7897ca494016ffe53ec831481
Andres Otarola <andres@otarola.me>
2019-10-16 00:58:53 -0500

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 # tag current build and push new images
 
-docker build -t $DOCKER_USERNAME/multi-client:latest -t $DOCKER_USERNAME/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t $DOCKER_USERNAME/multi-server:latest -t $DOCKER_USERNAME/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t $DOCKER_USERNAME/multi-worker:latest -t $DOCKER_USERNAME/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t aotarola/multi-client:latest -t aotarola/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aotarola/multi-server:latest -t aotarola/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aotarola/multi-worker:latest -t aotarola/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push $DOCKER_USERNAME/multi-client:latest
-docker push $DOCKER_USERNAME/multi-client:$SHA
-docker push $DOCKER_USERNAME/multi-server:latest
-docker push $DOCKER_USERNAME/multi-server:$SHA
-docker push $DOCKER_USERNAME/multi-worker:latest
-docker push $DOCKER_USERNAME/multi-worker:$SHA
+docker push aotarola/multi-client:latest
+docker push aotarola/multi-client:$SHA
+docker push aotarola/multi-server:latest
+docker push aotarola/multi-server:$SHA
+docker push aotarola/multi-worker:latest
+docker push aotarola/multi-worker:$SHA
 
 kubectl apply -f k8s
 
 # force deployments to update
-kubectl set image deployments/client-deployment server=$DOCKER_USERNAME/multi-server:$SHA
-kubectl set image deployments/server-deployment server=$DOCKER_USERNAME/multi-server:$SHA
-kubectl set image deployments/worker-deployment server=$DOCKER_USERNAME/multi-server:$SHA
+kubectl set image deployments/client-deployment client=aotarola/multi-server:$SHA
+kubectl set image deployments/server-deployment server=aotarola/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=aotarola/multi-server:$SHA
